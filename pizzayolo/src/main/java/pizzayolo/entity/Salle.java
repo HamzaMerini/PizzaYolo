3b9108200ee33a3d6f788059b40713b5ff14f505
Khadidja <Khadidjasoukehal15@gmail.com>
2022-04-28 17:11:25 +0200

    Repository,context,dao

--- pizzayolo/src/main/java/pizzayolo/entity/Salle.java
+++ pizzayolo/src/main/java/pizzayolo/entity/Salle.java
@@ -2,7 +2,7 @@ package pizzayolo.entity;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-import pizzayolo.util.Context;
+
 @Entity
 @DiscriminatorValue("S")
 public class Salle extends Commande{
@@ -27,29 +27,9 @@ public PlanSalle getTable() {
 	}
 
 
-public void voirTableLibre(){
-	
-		if (Context.getInstance().getTableLibre().isEmpty()) {System.out.println("Toutes les tables sont prises");}
-		else {
-		for (PlanSalle t : Context.getInstance().getTableLibre())
-	{
-		System.out.println(t);
-	}
-		}
-	}
-  
-  
-	public void prendreTable(PlanSalle PlanSalle){
-		Context.getInstance().getTablePrise().add(PlanSalle);
-		Context.getInstance().getTableLibre().remove(PlanSalle);
-		
-	}
-	
-	public void libererTable(PlanSalle PlanSalle){
-		Context.getInstance().getTableLibre().add(PlanSalle);
-		Context.getInstance().getTablePrise().remove(PlanSalle);
+
 	}
 
 	
 	
-}
+
