bac55b1403ca485346901b253cbb0521fb0a0cfa
Khadidja <Khadidjasoukehal15@gmail.com>
2022-06-02 17:24:25 +0200

    model
    dessert, commande dessert ,commande boisson

---
+++ pizzayoloangular/src/app/model/commandedessert.spec.ts
@@ -0,0 +1,7 @@
+import { Commandedessert } from './commandedessert';
+
+describe('Commandedessert', () => {
+  it('should create an instance', () => {
+    expect(new Commandedessert()).toBeTruthy();
+  });
+});
