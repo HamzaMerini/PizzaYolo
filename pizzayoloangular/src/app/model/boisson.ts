37880a3c8103c1d805b0c0e28545af15eeaaba99
HamzaMerini <64372438+HamzaMerini@users.noreply.github.com>
2022-06-03 11:44:42 +0200

    Update boisson.ts

--- pizzayoloangular/src/app/model/boisson.ts
+++ pizzayoloangular/src/app/model/boisson.ts
@@ -1,51 +1,30 @@
 export class Boisson {
-<<<<<<< Updated upstream
   public constructor(
     private _id?: number,
     private _nom?: string,
     private _prix?: number
   ) {}
-=======
-  public constructor(private _id?: number, private _nom?: string, private _prix?: number) {}
->>>>>>> Stashed changes
 
   get id(): number | undefined {
     return this._id;
   }
 
-<<<<<<< Updated upstream
   set id(value: number | undefined) {
     this._id = value;
   }
 
-=======
->>>>>>> Stashed changes
   get nom(): string | undefined {
     return this._nom;
   }
 
-<<<<<<< Updated upstream
   set nom(value: string | undefined) {
     this._nom = value;
   }
 
-=======
->>>>>>> Stashed changes
   get prix(): number | undefined {
     return this._prix;
   }
 
-<<<<<<< Updated upstream
-=======
-  set id(value: number | undefined) {
-    this._id = value;
-  }
-
-  set nom(value: string | undefined) {
-    this._nom = value;
-  }
-
->>>>>>> Stashed changes
   set prix(value: number | undefined) {
     this._prix = value;
   }
