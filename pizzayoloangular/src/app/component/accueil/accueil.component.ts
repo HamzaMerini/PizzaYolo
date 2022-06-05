6449c8b2d0e7189b52f141d673d6958f2d25040f
Hamza Merini <hamzammerini@gmail.com>
2022-06-05 13:06:38 +0200

    carousel OK

--- pizzayoloangular/src/app/component/accueil/accueil.component.ts
+++ pizzayoloangular/src/app/component/accueil/accueil.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css'],
+  styleUrls: [
+    '../../../assets/css/carousel.css',
+    '../../../assets/css/gmaps.css',
+    '../../../assets/css/test.css',
+    '../../../assets/css/font.css',
+    '../../../assets/css/style.css',
+    '../../../assets/vendor/animate.css/animate.min.css',
+  ],
 })
 export class AccueilComponent implements OnInit {
   constructor() {}
